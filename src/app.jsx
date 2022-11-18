680148d76ad5420129934799b76a9ca5558ed87e
kaynekaynekayne <theygoon@gmail.com>
2022-11-18 17:44:43 +0900

    making router and components

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,16 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      dd
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
