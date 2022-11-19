c7d0ac50305ceddb430dae4d22153be32a1615a7
kaynekaynekayne <theygoon@gmail.com>
2022-11-19 18:01:47 +0900

    delete edit is true or not

--- src/components/voca.jsx
+++ src/components/voca.jsx
@@ -4,13 +4,6 @@ const Voca = ({list, index, updateVocas}) => {
 
     const [newWords, setNewWords]=useState(list.words);
     const [newMeanings, setNewMeanings]=useState(list.meanings);
-    
-    const [isEdited, setIsEdited]=useState(false);
-
-    const handleUpdate=(e)=>{
-        e.preventDefault();
-        setIsEdited(prev=>!prev);
-    };
 
     const handleConfirm=(e)=>{
         e.preventDefault();
@@ -20,37 +13,22 @@ const Voca = ({list, index, updateVocas}) => {
 
     return (
         <div>
-            {isEdited ?  
-                <form>
-                    <input 
-                        value={newWords}
-                        onChange={(e)=>setNewWords(e.target.value)}
-                    />
-                    <input 
-                        value={newMeanings}
-                        onChange={(e)=>setNewMeanings(e.target.value)}
-                    />
-                    <button
-                        onClick={handleConfirm}
-                    >확인</button>
-                    <button>취소</button>
-                </form>
-            :
-                <form>
-                    <input 
-                        value={list.words}
-                        type="text"
-                        readOnly
-                    />
-                    <input 
-                        value={list.meanings}
-                        type="text"
-                        readOnly
-                    />
-                    <button onClick={handleUpdate}>수정</button>
-                    <button>삭제</button>
-                </form>
-            }
+            <form>
+                <input 
+                    value={newWords}
+                    onChange={(e)=>setNewWords(e.target.value)}
+                    required
+                />
+                <input 
+                    value={newMeanings}
+                    onChange={(e)=>setNewMeanings(e.target.value)}
+                    required
+                />
+                <button
+                    onClick={handleConfirm}
+                >변경</button>
+                <button>삭제</button>
+            </form>
         </div>
     )
 }
